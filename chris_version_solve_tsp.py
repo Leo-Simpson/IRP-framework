a1eb836e205d99389e203b67f5514afcc0ad0395
ChrisHTV <66363160+ChrisHTV@users.noreply.github.com>
2020-06-07 12:59:20 +0200

    Update chris_version_solve_tsp.py

--- chris_version_solve_tsp.py
+++ chris_version_solve_tsp.py
@@ -7,7 +7,7 @@ def tsp_tour(tour,solution):
     tour_temp = np.array([tour[0]] + tour)    #The tsp_solver can only solve for different start and endpoints, so I add the WH a second time.
     name_warehouses, name_schools = np.array(solution.name_warehouses), np.array(solution.name_schools)
     tour_in_names = np.concatenate([name_warehouses[tour_temp[:2]], name_schools[tour_temp[2:]]])
-    distance_matrix = solution.D.loc[tour_in_names, tour_in_names].values
+    distance_matrix = solution.problem.D.loc[tour_in_names, tour_in_names].values
 
     tsp_sol = solve_tsp(distance_matrix, endpoints = (0,1))            #the solver with the WH as start and endpoint
     
