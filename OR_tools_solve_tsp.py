22132f7de681b0bdf4eefe72e76c8933fa4daa75
ChrisHTV <66363160+ChrisHTV@users.noreply.github.com>
2020-06-07 14:20:18 +0200

    Add files via upload

---
+++ OR_tools_solve_tsp.py
@@ -0,0 +1,70 @@
+from __future__ import print_function
+from ortools.constraint_solver import routing_enums_pb2
+from ortools.constraint_solver import pywrapcp
+import numpy as np
+
+def create_data_model(distance_matrix):
+    """Stores the data for the problem."""
+    data = {}
+    data['distance_matrix'] = np.ndarray.tolist(distance_matrix)  # yapf: disable
+    data['num_vehicles'] = 1
+    data['depot'] = 0
+    return data
+
+
+def tsp_tour_comp(distance_matrix):
+    """Entry point of the program."""
+    # Instantiate the data problem.
+    data = create_data_model(distance_matrix)
+
+    # Create the routing index manager.
+    manager = pywrapcp.RoutingIndexManager(len(data['distance_matrix']),
+                                           data['num_vehicles'], data['depot'])
+
+    # Create Routing Model.
+    routing = pywrapcp.RoutingModel(manager)
+
+
+    def distance_callback(from_index, to_index):
+        """Returns the distance between the two nodes."""
+        # Convert from routing variable Index to distance matrix NodeIndex.
+        from_node = manager.IndexToNode(from_index)
+        to_node = manager.IndexToNode(to_index)
+        return data['distance_matrix'][from_node][to_node]
+
+    transit_callback_index = routing.RegisterTransitCallback(distance_callback)
+
+    # Define cost of each arc.
+    routing.SetArcCostEvaluatorOfAllVehicles(transit_callback_index)
+
+    # Setting first solution heuristic.
+    search_parameters = pywrapcp.DefaultRoutingSearchParameters()
+    search_parameters.first_solution_strategy = (
+        routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC)
+
+    # Solve the problem.
+    solution = routing.SolveWithParameters(search_parameters)
+    
+    tsp_route=[]
+    index = routing.Start(0)
+    route_distance = 0
+    while not routing.IsEnd(index):
+        tsp_route.append(manager.IndexToNode(index))
+        previous_index = index
+        index = solution.Value(routing.NextVar(index))
+        route_distance += routing.GetArcCostForVehicle(previous_index, index, 0)
+    index = routing.Start(0)
+    tsp_route.append(manager.IndexToNode(index))
+    
+    return tsp_route, route_distance
+    
+def tsp_tour(tour, solution):
+    tour_temp = np.array(tour)
+    name_warehouses, name_schools = np.array(solution.name_warehouses), np.array(solution.name_schools)
+    tour_in_names = np.concatenate([name_warehouses[tour_temp[:1]], name_schools[tour_temp[1:]]])
+    distance_matrix = solution.problem.D.loc[tour_in_names, tour_in_names].values
+    
+    tsp_sol, route_distance = tsp_tour_comp(distance_matrix)
+    
+    tsp_tour = tour_temp[tsp_sol]  #converting indices back from range(len(tour)) to range(M+N)
+    return tsp_tour, route_distance
