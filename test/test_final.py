3b7fe5b7dafdde3acd9b708d894c6d20271f7021
Sabi333 <66971800+Sabi333@users.noreply.github.com>
2020-07-08 22:41:07 +0200

    included different types of vehicles in GUI and ISI

--- test/test_final.py
+++ test/test_final.py
@@ -6,10 +6,10 @@ from ISI import random_problem, Matheuristic
 
 T, N, K,M = 5,2,4,15
 H = 5
-problem = random_problem(T,N,K,M, H = H,seed=1)
+problem = random_problem(T,N,M,K, H = H,seed=1)
 heuristic = Matheuristic(problem)
 heuristic.param.rho_percent = 0.3
 heuristic.param.cooling = 0.5
 
-heuristic.algo2(info = False, plot = False)
+heuristic.algo2(info = True, plot = False)
 
