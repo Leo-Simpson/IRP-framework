ab400b9d2bbf3103c22aff21c07ca7ee67a1bccd
Leo-Simpson <52931376+Leo-Simpson@users.noreply.github.com>
2020-07-13 15:59:10 +0200

    update

--- Design/main_cluster.py
+++ Design/main_cluster.py
@@ -163,26 +163,14 @@ class Window(QtWidgets.QMainWindow):
                                 t_load = self.t_load, c_per_km = self.c_per_km, Tmax = self.Tmax, V_number = self.V_number,
                                 central = self.central, makes = self.makes)
 
-            problem_global = problem_global.time_fuse(self.step_duration)
             
-            problems = problem_global.clustering()
             param = Meta_param(seed=1)
             param.tau_start = 3.
             param.tau_end = 1.
             param.cooling = 0.8
-            solutions = []
-            for counter, pr in enumerate(problems):
-                
-                print('Starting to compute cluster {} of {}! (Belonging to WH {})'.format(counter + 1, len(problems), pr.Warehouses[-1]['name']))
-                heuristic = Matheuristic(pr,param=param)
-                heuristic.algo2(plot_final=False, file = "solution/cluster %i.html" % (counter+1) )
-                solutions.append(heuristic.solution_best)
-                print('Cluster {} of {} computed! (Belonging to WH {})'.format(counter + 1, len(problems), pr.Warehouses[-1]['name']))
-            
-            solution = cluster_fusing(solutions,problem_global)
-            solution.file = "solution/global.html"
-            print(solution)
-            
+
+            problem_global.final_solver(param,time_step=self.step_duration)
+
             
 
 
